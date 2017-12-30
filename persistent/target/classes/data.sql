d542ed213179ffc8fabd967054cb379b7510a62d
Gediminas Kikilas <gkikilas@gmail.com>
2017-12-30 20:04:16 +0200

    mvn clean

--- persistent/target/classes/data.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO task (name, group_name) VALUES ('Task 1', 'Group 1');
-INSERT INTO task (name, group_name) VALUES ('Task 2', 'Group 1');
-INSERT INTO task (name, group_name) VALUES ('Sub-task 1', 'Group 2');
-INSERT INTO task (name, group_name) VALUES ('Sub-task 2', 'Group 2');
-INSERT INTO task_mapping (parent_id, child_id) VALUES (1, 3);
-INSERT INTO task_mapping (parent_id, child_id) VALUES (1, 4);
